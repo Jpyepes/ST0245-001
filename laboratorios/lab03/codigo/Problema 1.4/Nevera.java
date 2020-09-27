8f9f0bf924acad9593a75b9a12598a602e681a21
Jpyepes <61372991+Jpyepes@users.noreply.github.com>
2020-09-27 15:00:19 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/Problema 1.4/Nevera.java
@@ -0,0 +1,17 @@
+public class Nevera {
+    int codigo;
+    String marca;
+
+    public Nevera(int codigo, String marca){
+        this.codigo = codigo;
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Nevera{" +
+                "codigo=" + codigo +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
+}
